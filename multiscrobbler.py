4116ef5e4200c6252c2893e6951964c7d2f5ae12
Christopher Su <christophersu9@gmail.com>
2014-05-03 22:18:32 -0700

    removed comments

--- multiscrobbler.py
+++ multiscrobbler.py
@@ -9,11 +9,11 @@ class MultiScrobbler(object):
     def addAccount(self, user, password):
         password = pylast.md5(password)
         network = pylast.LastFMNetwork(api_key=Private.LASTFM_API_KEY, api_secret=Private.LASTFM_API_SECRET, username=user, password_hash=password)
-        print 'added user: ' + str(network)
+        # print 'added user: ' + str(network)
         self.accounts.append(network)
 
     def scrobbleAll(self, artist, song):
         now = int(time.time())
         for user in self.accounts:
-            print 'scrobbling: ' + artist + ' ' + song + ' to ' + str(user)
+            # print 'scrobbling: ' + artist + ' ' + song + ' to ' + str(user)
             user.scrobble(artist, song, now)
