3e52550ef9c4224a589dd018655aa8284f7cfc90
Christopher Su <christophersu9@gmail.com>
2014-05-03 22:18:23 -0700

    working multiscrobble

--- multiscrobbler.py
+++ multiscrobbler.py
@@ -6,9 +6,14 @@ class MultiScrobbler(object):
     def __init__(self):
         self.accounts = []
 
-    def addUser(self, user, password):
-        self.accounts.append(pylast.LastFMNetwork(api_key=Private.LASTFM_API_KEY, api_secret=Private.LASTFM_API_SECRET, username=user, password_hash=pylast.md5(password)))
+    def addAccount(self, user, password):
+        password = pylast.md5(password)
+        network = pylast.LastFMNetwork(api_key=Private.LASTFM_API_KEY, api_secret=Private.LASTFM_API_SECRET, username=user, password_hash=password)
+        print 'added user: ' + str(network)
+        self.accounts.append(network)
 
     def scrobbleAll(self, artist, song):
+        now = int(time.time())
         for user in self.accounts:
+            print 'scrobbling: ' + artist + ' ' + song + ' to ' + str(user)
             user.scrobble(artist, song, now)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='PyMultiScrobble',
+    version='1.0.0',
+    description='A Python module for scrobbling songs to multiple Last.fm accounts at once.',
+    author='Christopher Su',
+    packages=['PyMultiScrobble']
+    )
